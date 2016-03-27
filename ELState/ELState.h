b2af748d629711e78ac44c0474acdc2a2d9b0b2a
Brandon Sneed <brandon@redf.net>
2016-03-26 23:57:25 -0700

    Initial Commit

---
+++ ELState/ELState.h
@@ -0,0 +1,19 @@
+//
+//  ELState.h
+//  ELState
+//
+//  Created by Brandon Sneed on 3/26/16.
+//  Copyright © 2016 Electrode-iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ELState.
+FOUNDATION_EXPORT double ELStateVersionNumber;
+
+//! Project version string for ELState.
+FOUNDATION_EXPORT const unsigned char ELStateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ELState/PublicHeader.h>
+
+
